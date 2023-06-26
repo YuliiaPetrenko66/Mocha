a9917e4741c6b855931c399089d46cf34b5e6e6b
Yuliia Petrenko <juliapetrenko66@gmail.com>
2023-06-26 20:07:49 +0100

    Homework is updated

--- cypress/e2e/parametrizedHomeWork.cy.js
+++ cypress/e2e/parametrizedHomeWork.cy.js
@@ -18,16 +18,10 @@ describe('Parametrized test for "Using the Grid" form', ()=>{
   
     formCheck.forEach(({email,password}) => {
       it(`Test for text form "${email}"`, () => { 
-        // cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
-        // cy.get('[src="assets/images/material-dark-theme.jpg"]').click();
-        // cy.get('[title="Forms"]').click();
-        // cy.get('[title="Form Layouts"]').click();
+        
         cy.get('#inputEmail1').type(email);
         cy.get('#inputPassword2').type(password);
 
-        
-        // cy.get("nb-radio:nth-child(1) label:nth-child(1) span:nth-child(3)").click();
-        // cy.get("form.ng-untouched.ng-pristine.ng-valid div.form-group.row button[type='submit']").first().click();
     })
 
     afterEach(()=>{
