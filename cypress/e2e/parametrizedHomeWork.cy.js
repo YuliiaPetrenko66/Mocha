f627a5f99a11a5621c0e8c25e3514d3955e1d707
Yuliia Petrenko <juliapetrenko66@gmail.com>
2023-06-26 20:21:15 +0100

    Homework is updated #3

--- cypress/e2e/parametrizedHomeWork.cy.js
+++ cypress/e2e/parametrizedHomeWork.cy.js
@@ -8,7 +8,7 @@ describe('Parametrized test for "Using the Grid" form', ()=>{
     cy.get('[src="assets/images/material-dark-theme.jpg"]').click();
     cy.get('[title="Forms"]').click();
     cy.get('[title="Form Layouts"]').click();
-  })
+  });
 
     const formCheck = [
       {email:'YuliiaPetrenko@gmail.com', password:'Mother'},
@@ -18,17 +18,13 @@ describe('Parametrized test for "Using the Grid" form', ()=>{
   
     formCheck.forEach(({email,password}) => {
       it(`Test for text form "${email}"`, () => { 
-        
         cy.get('#inputEmail1').type(email);
         cy.get('#inputPassword2').type(password);
-
-    })
-
-    afterEach(()=>{
-      cy.get("nb-radio:nth-child(1) label:nth-child(1) span:nth-child(3)").click();
-      cy.get("form.ng-untouched.ng-pristine.ng-valid div.form-group.row button[type='submit']").first().click();
-    })
-  
-  }) 
-}
-  )
+      });
+      
+      afterEach(()=>{
+        cy.get("nb-radio:nth-child(1) label:nth-child(1) span:nth-child(3)").click();
+        cy.get("form.ng-untouched.ng-pristine.ng-valid div.form-group.row button[type='submit']").first().click();
+    });
+  });
+});
